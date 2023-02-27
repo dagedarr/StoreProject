545fc500b5587e8a4e16ef081167aa5dd04a8b8e
Natolin Artem <arnat64@yandex.ru>
2023-02-27 11:47:02 +0300

    Admin added

--- OnlineStore/store/admin.py
+++ OnlineStore/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price','old_price', 'tag_list')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+admin.site.register(Item, ItemAdmin)
