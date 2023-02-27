a3fb02fb6163140be4f7c2d1cd5884b249ad3c28
Natolin Artem <arnat64@yandex.ru>
2023-02-27 19:05:59 +0300

    added item_detail

--- OnlineStore/store/views.py
+++ OnlineStore/store/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Item
 
 def store(request):
     items = Item.objects.all()
     context = {'items': items}
     return render(request, 'store/main_page.html', context)
+
+def item_details(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    context = {'item': item}
+    return render(request, 'store/item_details.html', context)    
