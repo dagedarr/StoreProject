2898925d8547b7dd7b400709e79e474d9e63db9d
Natolin Artem <arnat64@yandex.ru>
2023-02-28 18:54:19 +0300

    added features

--- OnlineStore/store/views.py
+++ OnlineStore/store/views.py
@@ -3,10 +3,14 @@ from .models import Item
 
 def store(request):
     items = Item.objects.all()
-    context = {'items': items}
+    context = {'items': items,
+               'range': [*range(1,7)],
+    }
     return render(request, 'store/main_page.html', context)
 
 def item_details(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
-    context = {'item': item}
+    context = {
+        'item': item,
+        }
     return render(request, 'store/item_details.html', context)    
