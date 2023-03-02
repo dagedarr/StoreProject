343a433b7a78921bccc754845f7504b924833054
Natolin Artem <arnat64@yandex.ru>
2023-03-02 13:07:40 +0300

    added 'about'

---
+++ OnlineStore/about/views.py
@@ -0,0 +1,9 @@
+from django.views.generic.base import TemplateView
+
+
+class AboutAuthorView(TemplateView):
+    template_name = 'about/author.html'
+
+
+class AboutProjectView(TemplateView):
+    template_name = 'about/project.html' 
