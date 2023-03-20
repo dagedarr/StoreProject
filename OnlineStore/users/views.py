3c5cc05104144b83480847a5b574df1d2b036c8a
Natolin Artem <arnat64@yandex.ru>
2023-03-20 13:39:17 +0300

    added telegram bot

--- OnlineStore/users/views.py
+++ OnlineStore/users/views.py
@@ -1,8 +1,12 @@
+import telegram
+import asyncio
+
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.shortcuts import render
 
+from OnlineStore.settings import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 from checkout.models import Order
 from .forms import CreationForm, FeedbackForm
 from .models import Feedback
@@ -10,6 +14,9 @@ from .models import Feedback
 
 @login_required
 def user_orders(request):
+    """
+    Представление списка заказов пользователя.
+    """
     orders = Order.objects.filter(user=request.user)
     context = {
         'orders': orders,
@@ -19,6 +26,9 @@ def user_orders(request):
 
 @login_required
 def profile(request):
+    """
+    Представление профиля пользователя.
+    """
     return render(request, 'users/profile.html')
 
 
@@ -28,7 +38,19 @@ class SignUp(CreateView):
     template_name = 'users/signup.html'
 
 
+async def send_telegram_message(message):
+    """
+    Асинхронная функция для отправки сообщения в ТГ.
+    """
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    chat_id = TELEGRAM_CHAT_ID
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
 def feedback_processing(request):
+    """
+    Представление приема и обработки для обратной связи.
+    """
     if request.method == 'POST':
         form = FeedbackForm(request.POST)
         if form.is_valid():
@@ -38,5 +60,10 @@ def feedback_processing(request):
                 feedback_message=form.cleaned_data['feedback_message'],
             )
             feedback.save()
+
+            # Отпрака сообщения
+            message = f"Новое сообщение от {feedback.feedback_name} ({feedback.feedback_email}): {feedback.feedback_message}"
+            asyncio.run(send_telegram_message(message))
+
             return render(request, 'users/feedback_success.html')
     return render(request, 'users/feedback_failed.html')
