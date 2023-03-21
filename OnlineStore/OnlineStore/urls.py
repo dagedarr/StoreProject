e3da351083c5156d62e96cfd4148a61241758be5
Natolin Artem <arnat64@yandex.ru>
2023-03-21 17:52:45 +0300

    media added

--- OnlineStore/OnlineStore/urls.py
+++ OnlineStore/OnlineStore/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +13,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
