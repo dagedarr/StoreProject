0a9558b1ad451a57b507d447236763bc7abebe6f
Natolin Artem <arnat64@yandex.ru>
2023-03-21 18:01:43 +0300

    migrations deleted

--- OnlineStore/checkout/migrations/0002_initial.py
+++ OnlineStore/checkout/migrations/0002_initial.py
@@ -11,19 +11,24 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('cart', '0001_initial'),
+        ('checkout', '0001_initial'),
         ('store', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='cartitem',
+            model_name='orderitem',
             name='item',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='store.item', verbose_name='Товар'),
         ),
         migrations.AddField(
-            model_name='cart',
+            model_name='orderitem',
+            name='order',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='checkout.order', verbose_name='Заказ'),
+        ),
+        migrations.AddField(
+            model_name='order',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='carts', to=settings.AUTH_USER_MODEL, verbose_name='Покупатель'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='orders', to=settings.AUTH_USER_MODEL, verbose_name='Покупатель'),
         ),
     ]
