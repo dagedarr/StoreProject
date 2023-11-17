4fae08c574ba1d59fbc3e2c1c120c4cd09c8ecac
Natolin Artem <arnat64@yandex.ru>
2023-11-17 17:16:04 +0300

    refac

--- OnlineStore/OnlineStore/settings.py
+++ OnlineStore/OnlineStore/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
